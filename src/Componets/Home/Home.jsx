e4a81fd1fe0d0cd9b56a569a4f0e581f3dea3cc5
KBolotkanov <westbek9@gmail.com>
2022-05-27 23:29:51 +0600

    footer

--- src/Componets/Home/Home.jsx
+++ src/Componets/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
 import InfoPage from '../InfoPage/InfoPage';
 
 const Home = () => {
     return (
         <div>
             <InfoPage/>
+            <Footer/>
         </div>
     );
 };
