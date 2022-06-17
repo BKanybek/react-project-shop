086bea8060cecedd7008285728678a9c7448ba2a
workNote <westbek9@gmail.com>
2022-06-17 18:00:25 +0600

    infoPage css

--- src/Componets/InfoPage/InfoPage.jsx
+++ src/Componets/InfoPage/InfoPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { infoPageData } from '../data'
 import InfoPageItem from './InfoPageItem';
+import './InfoPageItem.css'
 
 
 const InfoPage = () => {
     return (
-        <div style={{display: 'flex', padding: '20px', maxWidth: '1360px', margin: 'auto'}}>
-            {infoPageData.map(item =>(
-                <InfoPageItem item={item}/>
+        <div className='wrapper'>
+         {infoPageData.map(item =>(
+            <InfoPageItem item={item}/>
             ))}
         </div>
     );
