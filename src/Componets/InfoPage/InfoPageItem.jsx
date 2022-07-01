560f05259405fa48b50226ebf5ccb4b64e787cdd
workNote <westbek9@gmail.com>
2022-07-01 11:42:42 +0600

    download

--- src/Componets/InfoPage/InfoPageItem.jsx
+++ src/Componets/InfoPage/InfoPageItem.jsx
@@ -5,12 +5,13 @@ const InfoPageItem = ({item}) => {
     console.log(item.img)
     return (
         <div className='container-wrapper'>
+            <div className='img-wrapper'>
                 <img className='img' src={item.img} alt='test'/>    
+            </div>
             <div className='info'>
                 <h1 className='title'>{item.title}</h1>
                 <a href='/' className='button'> Подробнее</a>
-            </div>
-            
+            </div>    
         </div>
     );
 };
