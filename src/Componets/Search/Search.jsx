f3c81696909b5818c7e6929177a57aeede6da73f
workNote <westbek9@gmail.com>
2022-07-22 16:42:36 +0600

    change search test

--- src/Componets/Search/Search.jsx
+++ src/Componets/Search/Search.jsx
@@ -1,27 +1,14 @@
-import React from 'react';
-import CloseIcon from '@mui/icons-material/Close';
-import SearchIcon from '@mui/icons-material/Search';
-import './Search.css'
-import { useState } from 'react';
+// import React from 'react';
 
-const Search = () => {
-    const [show, setShow] = useState(true)
+// import './Search.css'
+// import { useState } from 'react';
 
-    return (
-        <div className='search'>
-            <div className='search-wrapper'>
-                <div className={`search-input ${!show && 'search-input-active'}`}>
-                    <input className='input-search' type="text" placeholder="Поиск"/>
-                    <div className='cross-icon' onClick={() => setShow(!show)}>
-                        <CloseIcon/>
-                    </div>
-                </div>
-                <div className='search-icon'>
-                    <SearchIcon style={{fontSize: '30px'}} onClick={() => setShow(!show)}/>
-                </div>
-            </div>
-        </div>
-    );
-};
+// const Search = () => {
+   
 
-export default Search;
+//     return (
+       
+//     );
+// };
+
+// export default Search;
