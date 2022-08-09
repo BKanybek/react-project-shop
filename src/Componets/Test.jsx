a17964765836eba899918e13924711c762a8cffb
workNote <westbek9@gmail.com>
2022-08-09 17:31:58 +0600

    test200

---
+++ src/Componets/Test.jsx
@@ -0,0 +1,26 @@
+// import React, { useEffect, useState } from 'react';
+import { Button } from '@mui/material';
+import { useState } from 'react';
+import './Test.css'
+
+const Test = () => {
+    const [show, setShow] = useState(false)
+    console.log(show);
+
+
+    return (
+     <div className='test'>
+        <div className='first'>
+           <div className='two'>
+                <div className={`three ${!show && 'three-active'}`}>
+                </div>
+                
+
+           </div>
+        </div>
+        <button onClick={() => setShow(!show)}>Нажми</button>
+     </div>   
+    );
+}
+
+export default Test
