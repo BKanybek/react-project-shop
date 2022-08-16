483231d8080e5c73621b870b2475396d1eeb5acf
KBolotkanov <westbek9@gmail.com>
2022-08-16 20:16:26 +0600

    test233231

--- src/Componets/Navbar/Navbar.jsx
+++ src/Componets/Navbar/Navbar.jsx
@@ -21,10 +21,10 @@ export default function NavBar() {
   const [show, setShow] = useState(true)
 
   const {products, getProducts} = useContext(productContext)
+  console.log(products);
 
   const[filteredData, setFilteredData] = useState([])
-  const data = products.map([])
-  console.log(data);
+
   const handleFilter = (event) => {
     const searchWord = event.target.value
     const newFilter = products.filter((value) => {
@@ -43,9 +43,9 @@ export default function NavBar() {
     else document.body.style.overflow = 'visible';
   }, [!Mobile])
 
-  useEffect(() => {
-    getProducts()
-  }, [])
+  // useEffect(() => {
+  //   getProducts()
+  // }, [])
   
 
 
